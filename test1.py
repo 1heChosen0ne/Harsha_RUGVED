239ceaff02f2c43cd3e912f2b8b9460983c931d8
1heChosen0ne <vihasithsanam@gmail.com>
2025-09-28 19:55:52 +0530

    Remove old root paths from Git, keep files in task_1

--- test1.py
+++
@@ -1,7 +0,0 @@
-#take str input and count number of words
-string = str(input("enter the sentence: "))
-x = 0
-for i in string:
-    if (i==" "):
-        x += 1
-print("the number of words are ", x+1)
