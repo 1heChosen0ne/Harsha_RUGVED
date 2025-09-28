0a73cd8c76268083340ed494e67da0d51881f270
1heChosen0ne <vihasithsanam@gmail.com>
2025-09-28 13:54:17 +0530

    Initial commit

---
+++ Three_parameters.py
@@ -0,0 +1,6 @@
+a = input("Enter true/false for first statement: ")
+b = input("Enter true/false for second statement: ")
+c = input("Enter true/false for third statement: ")
+def tripleand(a, b, c):
+    return a and b and c
+print(tripleand(a, b, c))
